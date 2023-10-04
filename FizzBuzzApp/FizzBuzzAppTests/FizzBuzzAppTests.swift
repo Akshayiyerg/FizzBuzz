cca622540df6daeabebec9b949d86ae54f97b8fe
Akshay Ganesh <akshayiyerg@gmail.com>
2023-10-04 17:21:22 -0400

    Test case which tests FizzBuzz prints Fizz for multiple of 3 and minor changes in helper function.

--- FizzBuzzApp/FizzBuzzAppTests/FizzBuzzAppTests.swift
+++ FizzBuzzApp/FizzBuzzAppTests/FizzBuzzAppTests.swift
@@ -12,7 +12,7 @@ class FizzBuzzApp {
     
     func print(number: Int) -> String {
         
-        if number == 3 {
+        if number % 3 == 0 {
             return "Fizz"
         }
         return "\(number)"
@@ -25,23 +25,32 @@ final class FizzBuzzAppTests: XCTestCase {
         
         let sut = makeSUT()
         
-        expect(sut: sut, number: 1)
+        expect(sut: sut, number: 1, withResult: "1")
     }
     
     func test_printNumber_Two() {
         
         let sut = FizzBuzzApp()
         
-        expect(sut: sut, number: 2)
+        expect(sut: sut, number: 2, withResult: "2")
     }
     
     func test_printNumber_Three() {
         
         let sut = FizzBuzzApp()
         
-        let result = sut.print(number: 3)
+        expect(sut: sut, number: 3, withResult: "Fizz")
+    }
+    
+    func test_printFizz_multipleForThree() {
         
-        XCTAssertEqual(result, "Fizz")
+        let sut = FizzBuzzApp()
+        
+        let multipleThree = [6, 9, 12]
+        
+        multipleThree.forEach { element in
+            expect(sut: sut, number: element, withResult: "Fizz")
+        }
     }
 
     
@@ -51,10 +60,10 @@ final class FizzBuzzAppTests: XCTestCase {
         return FizzBuzzApp()
     }
     
-    func expect(sut: FizzBuzzApp, number: Int, file: StaticString = #filePath, line: UInt = #line) {
+    func expect(sut: FizzBuzzApp, number: Int, withResult expectedResult: String, file: StaticString = #filePath, line: UInt = #line) {
         
         let result = sut.print(number: number)
         
-        XCTAssertEqual(result, "\(number)")
+        XCTAssertEqual(result, expectedResult)
     }
 }
